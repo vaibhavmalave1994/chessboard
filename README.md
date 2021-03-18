ace6c2e96ca219a34452d1cbc4e5696f0abb0bdd
Vaibhav Malave <vaibhavmalave@ayopop.com>
2021-03-18 19:08:09 +0530

    written console application and read me

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 
 To run program use
 go run main.go
+
+piece name 
+king
+queen
+pawn
+rook
+bishop
+knight
+
+position 
+A,B,C,D,E,F,G,H (letter must be capital)
+and number from 1-8
+
+
+vaibhavmalave@ip-192-168-0-6 chessboard % go run main.go
+1. Next Piece, 2. Bye
+1
+enter piece name 
+king
+enter piece position 
+A1
+predicted position [B1 A2 B2]
+1. Next Piece, 2. Bye
+1
+enter piece name 
+queen
+enter piece position 
+D5
+predicted position [E5 F5 G5 H5 C5 B5 A5 D4 D3 D2 D1 D6 D7 D8 E6 F7 G8 E4 F3 G2 H1 C4 B3 A2 C6 B7 A8]
+1. Next Piece, 2. Bye
+2  
+BYE
+vaibhavmalave@ip-192-168-0-6 chessboard % 
--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import(
 )
 
 func main(){
-	knight := chess.SetPiece("knight", "H8")
-	fmt.Println("predicted position", knight.PredictMoves())
+	
+	var choice int
+	var pieceName string
+	var position string
+cont:
+	fmt.Println("1. Next Piece, 2. Bye")
+	fmt.Scanf("%d\n", &choice)
+	
+
+	switch choice {
+	case 1:
+		fmt.Println("enter piece name ")
+		fmt.Scanf("%s\n", &pieceName)
+		fmt.Println("enter piece position ")
+		fmt.Scanf("%s\n", &position)
+		knight := chess.SetPiece(pieceName,position)
+		fmt.Println("predicted position", knight.PredictMoves())
+		goto cont
+	default:
+		fmt.Println("BYE")
+	}
 }
